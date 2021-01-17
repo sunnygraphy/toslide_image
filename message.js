98e247c0b45506f20f73f0ea0ba5898828fa0364
prefer2 <sunho6200@gmail.com>
2021-01-17 17:22:31 +0900

    button link

---
+++ message.js
@@ -0,0 +1,18 @@
+function movePagePhoto(){
+    if(window.innerWidth>1366){
+      location.href = "https://prefer2.github.io/toslide_image/index_ko.html";
+    }   
+    else{
+      location.href = "https://prefer2.github.io/toslide_image/mobile_ko.html";
+    }
+  }
+
+  function movePageWrite(){
+    location.href = "https://prefer2.github.io/toslide_image/message.html";
+  }
+
+  function movePageMessage(){
+    location.href = "https://prefer2.github.io/toslide_image/show_message.html";
+  }
+
+ 
