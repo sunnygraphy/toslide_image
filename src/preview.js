7dbfc7de516123ac98f58d68669c53c90358f95d
sunnygraphy <41325787+sunnygraphy@users.noreply.github.com>
2021-01-22 13:00:05 +0900

    Update preview.js

--- src/preview.js
+++ src/preview.js
@@ -2,6 +2,12 @@ function previewImage(f){
 
 	var file = f.files;
 
+	//사진 한번 불러들이고, 다시 화면 열고  취소하면 에러납니다
+	// 아래 코드 추가 해주세요
+	console.log(f.files.length);
+	if(f.files.length ==0)
+	 return;
+	
 	// 확장자 체크
 	if(!/\.(jpg|jpeg|png)$/i.test(file[0].name)){
 		alert('jpg, png 파일만 선택해 주세요.\n\n현재 파일 : ' + file[0].name);
